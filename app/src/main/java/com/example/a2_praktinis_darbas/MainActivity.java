a1566a00e0d1b9c6fa01c1221213e784de20cd77
Evelinuxas <159789394+Evelinuxas@users.noreply.github.com>
2024-10-18 19:58:08 +0300

    Tesiamas darbas

--- app/src/main/java/com/example/a2_praktinis_darbas/MainActivity.java
+++ app/src/main/java/com/example/a2_praktinis_darbas/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.a2_praktinis_darbas;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edUserInput;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        EditText edUserInput = findViewById(R.id.edUserInput);
+        TextView tvResult = findViewById(R.id.tvResult);
+        Spinner spinner = findViewById(R.id.spCountOptions);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.counting_options,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    public void onBtnCountClick(View view) {
+
     }
 }
---
+++ app/src/main/java/com/example/a2_praktinis_darbas/TextCounter.java
@@ -0,0 +1,10 @@
+package com.example.a2_praktinis_darbas;
+
+public class TextCounter {
+
+    public int getCharsCount(String inputPhrase){
+        return inputPhrase.length();
+
+
+    }
+}
